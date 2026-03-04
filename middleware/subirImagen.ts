5b038332f00f530eb0d89faf33b118faf3459b0f
PedroSuarez1402 <pssuarez1402@gmail.com>
2026-03-04 11:31:14 -0500

    arreglo 3 del subir imagen

--- middleware/subirImagen.ts
+++ middleware/subirImagen.ts
@@ -1,12 +1,19 @@
+// -------------------------------------------------------------------
+// 🚀 Middleware de Carga de Archivos con Multer
+// Este archivo configura 'multer' para gestionar la subida de archivos.
+// Define el destino de almacenamiento y genera un nombre de archivo único
+// para cada imagen, evitando colisiones y manteniendo la extensión original.
+// -------------------------------------------------------------------
 import multer from 'multer';
 import path from 'path';
 import { Request } from 'express';
 import { generarId } from '../helpers/tokens.js';
 
 const storage = multer.diskStorage({
-    destination: function(req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) {
-        cb(null, './public/uploads/');
-    },
+    // ¡Magia aquí! Al pasar un simple string en lugar de una función, 
+    // Multer se encarga de crear la carpeta si no existe.
+    destination: './public/uploads/',
+    
     filename: function(req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) {
         cb(null, generarId() + path.extname(file.originalname));
     }
